a7338a2380430668c86fa82253f1329a5bdfde24
Yohanes Amadeo Marvell <127278310+amadeomarvell@users.noreply.github.com>
2023-12-16 10:12:19 +0700

    Update config.py
    change path to v-5 dataset

--- config.py
+++ config.py
@@ -4,17 +4,17 @@ from yacs.config import CfgNode
 _C = CfgNode()
 
 _C.DATASET_DIR = '/content/DETR-Car-Logo-Detection/Car_Logo_Dataset'
-_C.IMAGE_DIR = os.path.join(_C.DATASET_DIR, 'Car_Logo_Dataset_Images')
+_C.IMAGE_DIR = os.path.join(_C.DATASET_DIR, 'Car_Logo_Dataset_V_5_Images')
 _C.ANNOT_FILE = os.path.join(
-    _C.DATASET_DIR, 'Car Logo Dataset V-3 Annotations DETR.txt')
+    _C.DATASET_DIR, 'Car Logo Dataset V-5 Annotations DETR.txt')
 _C.CROPPED_ANNOT_FILE = os.path.join(
-    _C.DATASET_DIR, 'Car Logo Dataset V-3 Training Set Annotations Cropped.txt')
+    _C.DATASET_DIR, 'Car Logo Dataset V-5 Training Set Annotations Cropped.txt')
 _C.CROPPED_ANNOT_FILE_TEST = os.path.join(
-    _C.DATASET_DIR, 'Car Logo Dataset V-3 Test Set Annotations Cropped.txt')
+    _C.DATASET_DIR, 'Car Logo Dataset V-5 Test Set Annotations Cropped.txt')
 
 _C.CLASS_NAMES = ['Audi', 'Chrysler', 'Citroen', 'GMC', 'Honda', 'Hyundai', 'Infiniti', 'Mazda', 'Mercedes', 'Mitsubishi', 
                   'Nissan', 'Renault', 'Toyota', 'Volkswagen', 'acura', 'bmw', 'cadillac', 'chevrolet', 'dodge', 'ford', 'jeep', 
-                  'kia', 'lexus', 'lincoln', 'mini', 'porsche', 'ram', 'range-rover', 'skoda', 'subaru', 'suzuki', 'volvo']
+                  'kia', 'lexus', 'license-plate', 'lincoln', 'mini', 'porsche', 'ram', 'range-rover', 'skoda', 'subaru', 'suzuki', 'volvo']
 
 
 def get_cfg_defaults():
